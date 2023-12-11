f410cbca4c22c7660bee7f5e10add0cf793e4e20
Francisco Carmona <flcarmonac@yahoo.com>
2023-12-11 17:30:38 -0300

    update

--- src/app/components/observador/observador.component.ts
+++ src/app/components/observador/observador.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { AuthService } from '../../services/auth.service';
 import { CommonModule } from '@angular/common';
 import { ProductsService } from '../../services/products.service';
 import { Product } from '../../interfaces/product';
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,6 @@
-export const environment = {};
+export const environment = {
+  production: true,
+  API_KEY: "Esmerilemelo-666",
+  API_URL: "https://bsite.net/metalflap",
+  GOOGLE_CLIENT_ID: "529904203616-rpd70g5dqapdh57aptbk01k6tkmgsppk.apps.googleusercontent.com",
+};
